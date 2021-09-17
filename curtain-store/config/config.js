f1f9686ee7468dcbb133608096b845745eb1026b
李通 <maggieq8324@gmail.com>
2021-09-17 18:45:49 +0800

    update config.js

--- curtain-store/config/config.js
+++ curtain-store/config/config.js
@@ -1,8 +1,5 @@
 const config = {
-    // apiBaseUrl: 'http://localhost:8080/cms-pictures',
-    // apiBaseUrl: 'http://192.168.0.103:8080/cms-pictures',
-    // apiBaseUrl: 'http://192.168.0.103:8081/v1',
-    apiBaseUrl: 'https://120.26.89.60/serve',
+    apiBaseUrl: 'http://localhost/serve',
     appkey: 'wx773b260ad2ca5502',
 }
 
